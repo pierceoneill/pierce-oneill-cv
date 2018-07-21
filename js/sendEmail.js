4b24582bd2db6849bd67d9b258ab06fe4bab5bac
Pierce ONeill <pierceoneillky@gmail.com>
2018-07-21 23:28:36 +0000

    update blog

---
+++ js/sendEmail.js
@@ -0,0 +1,20 @@
+
+var form = $("form#form");
+form.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "pierce";
+
+  form.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,"form")
+  	.then(function(){ 
+    	alert("Sent!");
+       form.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       form.find("button").text("Send");
+    });
+  return false;
+});
